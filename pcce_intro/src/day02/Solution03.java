e6ebe719033b80ad3956d106659547054777a2ab
Jayden6849 <jayden6849@gmail.com>
2024-12-04 12:51:25 +0900

    [Jayden] commit Solution3 (pcce_intro.day02)
    	- commit Solution3

--- pcce_intro/src/day02/Solution03.java
+++ pcce_intro/src/day02/Solution03.java
@@ -4,15 +4,26 @@ public class Solution03 {
 	public int[] solution(int numer1, int denom1, int numer2, int denom2) {
 		int[] answer = new int[2];
 		
-		int i = 1;
-		for(i=1; i<1000; i++) {
-			if (i%denom1 == 0 && i%denom2 == 0) {
-				answer[1] = i;
-				answer[0] = numer1 * (answer[1]/denom1) + numer2 * (answer[1]/denom2);
-				break;
-			}
-		}
-				
+		int numer = numer1 * denom2 + numer2 * denom1;
+		int denom = denom1 * denom2;
+		
+		int gcd = gcd(numer, denom);
+		
+		numer /= gcd;
+		denom /= gcd;
+		
+		answer[0] = numer;
+		answer[1] = denom;
+		
         return answer;
     }
+	public int gcd(int a, int b) {
+		while(b != 0) {
+			int r = a%b;
+			a = b;
+			b = r;
+		}
+		
+		return a;
+	}
 }
