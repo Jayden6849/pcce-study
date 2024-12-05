0ad6acb04168df6161a2fb14eff926dce96f2c16
Jayden6849 <jayden6849@gmail.com>
2024-12-05 10:14:27 +0900

    [Jayden] Create pkg_day04 (pcce_intro)
    	- create Run
    	- create Solution01-04

---
+++ pcce_intro/src/day04/Run.java
@@ -0,0 +1,15 @@
+package day04;
+
+public class Run {
+	public static void main(String[] args) {
+		// 1. 피자 나눠 먹기 (1)
+		System.out.println("=== 1번 ===");
+		// 2. 피자 나눠 먹기 (2)
+		System.out.println("=== 2번 ===");
+		// 3. 피자 나눠 먹기 (3)
+		System.out.println("=== 3번 ===");
+		// 4. 배열의 평균값
+		System.out.println("=== 3번 ===");
+		
+	}
+}
