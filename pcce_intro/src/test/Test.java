068215eddb3c72308a6f6bcfbe2861adf15188f6
Jayden6849 <jayden6849@gmail.com>
2024-12-05 09:07:18 +0900

    [Jayden] day03.Solution03 (solving)
    	- edit Test

--- pcce_intro/src/test/Test.java
+++ pcce_intro/src/test/Test.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Test {
 	public static void main(String[] args) {
-		int[] array = {1, 2, 3, 3, 3, 4, 3};
+		int[] array = {1, 2, 3, 3, 3, 4};
 		
 		int[] count = new int[2000];
 		int n;
@@ -18,8 +18,20 @@ public class Test {
 		
 		int max = count[0];
 		for(int i=0; i<count.length; i++) {
-			if(max < count[i]) max = count[i];
+			if(max<count[i]) max = count[i];
 		}
+		
+		System.out.println(max);
+		
+		int result = -1;
+		
+		for(int i=0; i<count.length; i++) {
+			if(max == count[i]) {
+				result = i;
+			} 
+		}
+		
+		System.out.println(result);
 		
 	}
 }
