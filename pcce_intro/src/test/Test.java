8124632b6e7391b956fd888f165ba513e5e61c69
Jayden6849 <jayden6849@gmail.com>
2024-12-11 12:09:02 +0900

    [Jayden] solving pkg_day09.solution03
    	- unsolved yet

--- pcce_intro/src/test/Test.java
+++ pcce_intro/src/test/Test.java
@@ -2,6 +2,26 @@ package test;
 
 public class Test {
 	public static void main(String args[]) {
-
+			int balls = 100;
+			int share = 99;
+			
+			long lBalls = balls;
+			long lshare = share;
+			
+			int answer = 1;
+			
+//			answer = (factorial(balls) / (double)((factorial(share) * factorial(balls-share))));
+		
+			System.out.println(factorial(30));
+			
+			System.out.println(answer);
 	  }
+	
+	private static long factorial(int n) {
+        long result = 1;
+        for(long i=1; i<=n; i++) { 
+            result *= i;
+        }
+        return result;
+    }
 }
