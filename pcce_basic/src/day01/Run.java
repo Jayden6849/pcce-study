2a99429944afc66d6c98c1879a6f3b8a7dd4f812
Jayden <jayden6849@gmail.com>
2025-05-25 08:12:15 +0900

    [cjs] chore : pcce_basic 프로젝트 - 1st comnit

---
+++ pcce_basic/src/day01/Run.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		// 1. 
+		System.out.println("===== 1번 ======");
+		Solution01 s1 = new Solution01();
+		
+		// 2. 
+		System.out.println("===== 2번 ======");
+		Solution02 s2 = new Solution02();
+		
+		// 3. 
+		System.out.println("===== 3번 ======");
+		Solution03 s3 = new Solution03();
+		
+		// 4. 
+		System.out.println("===== 4번 ======");
+		Solution04 s4 = new Solution04();
+	}
+	
+}
