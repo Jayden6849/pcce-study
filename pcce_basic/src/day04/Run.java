9b3743150a9ac28f9f93350955767a977113e922
Jayden6849 <jayden6849@gmail.com>
2025-06-24 13:49:39 +0900

    [Jay] feat: day04 풀이 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day04/Run.java
@@ -0,0 +1,23 @@
+package day04;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. n의 배수
+        Solution01 s1 = new Solution01();
+
+        // 2. 공배수
+        Solution02 s2 = new Solution02();
+
+        // 3. 홀짝에 따라 다른 값 반환하기
+        Solution03 s3 = new Solution03();
+
+        // 4. 조건 문자열
+        Solution04 s4 = new Solution04();
+
+        // 5. flag에 따라 다른 값 반환하기
+        Solution05 s5 = new Solution05();
+
+    }
+
+}
