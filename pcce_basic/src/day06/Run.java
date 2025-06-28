9c3d8d8d5ca4cbdc01f0add97ff2e8aeae0d26be
Jayden6849 <jayden6849@gmail.com>
2025-06-28 22:56:26 +0900

    [Jay] feat: day06 풀이 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day06/Run.java
@@ -0,0 +1,23 @@
+package day06;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. 마지막 두 원소
+        Solution01 s1 = new Solution01();
+
+        // 2. 수 조작하기 1
+        Solution02 s2 = new Solution02();
+
+        // 3. 수 조작하기 2
+        Solution03 s3 = new Solution03();
+
+        // 4. 수열과 구간 쿼리 3
+        Solution04 s4 = new Solution04();
+
+        // 5. 수열과 구간 쿼리 2
+        Solution05 s5 = new Solution05();
+
+    }
+
+}
