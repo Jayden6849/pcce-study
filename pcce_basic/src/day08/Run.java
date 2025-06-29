52096ef6553885708ade9e8d78a8a72099bc69ae
Jayden6849 <jayden6849@gmail.com>
2025-06-29 09:41:54 +0900

    [Jay] feat: day08 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day08/Run.java
@@ -0,0 +1,22 @@
+package day08;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. 간단한 논리 연산
+        Solution01 s1 = new Solution01();
+
+        // 2. 주사위 게임 3
+        Solution02 s2 = new Solution02();
+
+        // 3. 글자 이어 붙여 문자열 만들기
+        Solution03 s3 = new Solution03();
+
+        // 4. 9로 나눈 나머지
+        Solution04 s4 = new Solution04();
+
+        // 5. 문자열 여러 번 뒤집기
+        Solution05 s5 = new Solution05();
+    }
+
+}
