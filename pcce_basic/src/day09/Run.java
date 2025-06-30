aa713525df64ba144df62be2de0ca1a758ec481f
Jayden6849 <jayden6849@gmail.com>
2025-06-30 23:05:59 +0900

    [Jay] feat: day09 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day09/Run.java
@@ -0,0 +1,23 @@
+package day09;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. 배열 만들기 5
+        Solution01 s1 = new Solution01()
+
+        // 2. 부분 문자열 이어 붙여 문자열 만들기
+        Solution02 s2 = new Solution02();
+
+        // 3. 문자열의 뒤의 n글자
+        Solution03 s3 = new Solution03();
+
+        // 4. 접미사 배열
+        Solution04 s4 = new Solution04();
+
+        // 5. 접미사인지 확인하기
+        Solution05 s5 = new Solution05();
+
+    }
+
+}
