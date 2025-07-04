410a096b43713981ccc74cd5cbb445b1edbc50a5
Jayden6849 <jayden6849@gmail.com>
2025-07-04 09:48:13 +0900

    [Jay] feat: 10 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day10/Run.java
@@ -0,0 +1,23 @@
+package day10;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. 문자열의 앞의 n글자
+        Solution01 s1 = new Solution01();
+
+        // 2. 접두사인지 확인하기
+        Solution02 s2 = new Solution02();
+
+        // 3. 문자열 뒤집기
+        Solution03 s3 = new Solution03();
+
+        // 4. 세로 읽기
+        Solution04 s4 = new Solution04();
+
+        // 5. qr code
+        Solution05 s5 = new Solution05();
+
+    }
+
+}
