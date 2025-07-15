97038b0f64b0e7f78ea6dc1a97977c93a0c611c3
Jayden6849 <jayden6849@gmail.com>
2025-07-15 09:33:55 +0900

    [Jay] feat: 11 풀이 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day11/Run.java
@@ -0,0 +1,23 @@
+package day11;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. 문자 개수 세기
+        Solution01 s1 = new Solution01();
+
+        // 2. 배열 만들기 1
+        Solution02 s2 = new Solution02();
+
+        // 3. 글자 지우기
+        Solution03 s3 = new Solution03();
+
+        // 4. 카운트 다운
+        Solution04 s4 = new Solution04();
+
+        // 5. 가까운 1 찾기
+        Solution05 s5 = new Solution05();
+
+    }
+
+}
