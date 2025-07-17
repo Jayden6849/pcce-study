3f4ab6ee99c68e4b119bdaea1a7be544c8493328
Jayden6849 <jayden6849@gmail.com>
2025-07-17 14:28:59 +0900

    [Jay] feat: 12 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day12/Run.java
@@ -0,0 +1,18 @@
+package day12;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. 리스트 자르기
+        Solution01 s1 = new Solution01();
+        // 2. 첫 번째로 나오는 음수
+        Solution02 s2 = new Solution02();
+        // 3. 배열 만들기 3
+        Solution03 s3 = new Solution03();
+        // 4. 2의 영역
+        Solution04 s4 = new Solution04();
+        // 5. 배열 조각하기
+        Solution05 s5 = new Solution05();
+    }
+
+}
