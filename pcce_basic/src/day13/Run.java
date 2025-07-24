e0ed1cab85ba8ddd6c9645681e43b663a8846849
Jayden6849 <jayden6849@gmail.com>
2025-07-24 11:11:59 +0900

    [Jay] feat: 13 풀이 준비
    Signed-off-by: Jayden6849 <jayden6849@gmail.com>

---
+++ pcce_basic/src/day13/Run.java
@@ -0,0 +1,17 @@
+package day13;
+
+public class Run {
+
+    public static void main(String[] args) {
+        // 1. n번째 원소부터
+
+        // 2. 순서 바꾸기
+
+        // 3. 왼쪽 오른쪽
+
+        // 4. n번째 원소까지
+
+        // 5. n개 간격의 원소들
+    }
+
+}
